a582cfe9ebaaf4c096f6ec89cff301d97dda2ab1
FredPosch <f.poschenrieder@outlook.de>
2024-11-17 22:18:34 +0100

    I added the Input Layer explicit layer to comply with the recommended version and remove the warning, that previously occurred

--- app/model.py
+++ app/model.py
@@ -1,14 +1,14 @@
 # app/model.py
 import os
-from keras import Sequential, layers
+from keras import Sequential, layers, Input
 from keras.models import load_model
 
 def build_model(input_shape=(28, 28, 1), num_classes=10):
     model = Sequential(
         [
+            Input(shape=input_shape),
             layers.Conv2D(
-                32, kernel_size=(3, 3), activation="relu", input_shape=input_shape
-            ),
+                32, kernel_size=(3, 3), activation="relu"),
             layers.MaxPooling2D(pool_size=(2, 2)),
             layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
             layers.MaxPooling2D(pool_size=(2, 2)),
